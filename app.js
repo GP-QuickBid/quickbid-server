f6b9965edeb98f7d39e89c1ff298db83eaf3e9fa
Reza arga <reza.arga29@gmail.com>
2024-04-24 22:31:25 +0700

    testingsocket

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const express = require("express");
 const app = express();
 const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
 
 app.use(cors());
 
@@ -15,7 +20,11 @@ app.use(express.json());
 app.use(require("./routers"));
 app.use(errorHandler);
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
